8251eb91c258c9181edaf0082f34fe574a96b459
philipuuu <topgun0415@naver.com>
2024-07-01 01:38:26 +0900

    💄 Style : Button's style fixed

--- blog-site/src/components/Button.jsx
+++ blog-site/src/components/Button.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text }) => {
   return (
-    <button
-      onClick={onClick}
-      className="mt-4 px-6 py-3 bg-gradient-to-r bg-orange-400 bg-orange-400 bg-orange-400 text-white rounded-full shadow-md hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-gray-600"
-      style={{
-        textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)',
-      }}
-    >
+    <button className='flex w-full justify-center mt-5 rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
       {text}
     </button>
   );
