d37bb9dad252d4bfba5540da9998330dafbc018c
philipuuu <topgun0415@naver.com>
2024-07-01 01:48:21 +0900

    🔧 Fixed : delete react's import

--- blog-site/src/components/Button.jsx
+++ blog-site/src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Button = ({ text }) => {
   return (
     <button className='flex w-full justify-center mt-5 rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
