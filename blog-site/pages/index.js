f4f13eaa9ea05f81bd67f094121f49b54aedaafb
philipuuu <topgun0415@naver.com>
2024-07-01 01:45:35 +0900

    ✏️ Fix : Title fixed

--- blog-site/pages/index.js
+++ blog-site/pages/index.js
@@ -1,10 +1,10 @@
-// pages/index.js
 import LoginPage from '../src/login/LoginPage';
 
 export default function Home() {
   return (
     <>
       <div>
+        <title>Share Your Best Moments | BlogSite</title>
         <LoginPage />
       </div>
     </>
